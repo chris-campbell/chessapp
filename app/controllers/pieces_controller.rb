3d1b58f103d653e60a1cdafce3f04756a58b2794
Kris Camp <christopher.campbell@outlook.com>
2018-11-28 01:00:36 +0000

    Clean up rubocop violations in pieces controllers

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,6 @@
 class PiecesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def move
     @piece = Piece.find(params[:piece_id])
     @game = Game.find(params[:game_id])
@@ -8,29 +8,30 @@ class PiecesController < ApplicationController
     x = params[:position_x].to_i
     y = params[:position_y].to_i
 
-    if @piece.player == current_user.id
+    return false unless @piece.player == current_user.id
 
       if @piece.valid_move?(x, y)
         @piece.capture!(x, y)
+
         return false if @piece.update_turn(color)
+
         if @piece
-          ActionCable.server.broadcast 'piece_channel', position_x:  x,
-                      position_y: y, color: color,
-                      player: @piece.player, piece_id: @piece.id,
-                      turn: @game.opposite_color(@game.turn)
-        
+          ActionCable.server.broadcast 'piece_channel',
+                                        position_x: x,
+                                        position_y: y,
+                                        color: color,
+                                        player: @piece.player,
+                                        piece_id: @piece.id,
+                                        turn: @game.opposite_color(@game.turn)
         end
-        
       else
         false
       end
-    end
-	end
+  end
 
   private
 
   def piece_params
     params.require(:piece).permit(:position_x, :position_y, :game_id)
   end
-
 end
