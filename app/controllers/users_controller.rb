31ce8b27cf50791ecf3274347c3e407527b4acca
Kris Camp <christopher.campbell@outlook.com>
2019-01-04 13:40:31 +0000

    white listed username in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,6 @@ class UsersController < ApplicationController
     end
     
     def user_params
-      params.require(:user).permit(:email, :avatar)
+      params.require(:user).permit(:email, :avatar, :username)
     end
 end
