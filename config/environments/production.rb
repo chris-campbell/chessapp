7cb1f493b597f6878f9b1f6cea87e16d06021485
Kris Camp <christopher.campbell@outlook.com>
2019-01-06 12:40:50 +0000

    Add mailer configurations to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,17 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://chessmate-saturday-slackers.herokuapp.com' }
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
