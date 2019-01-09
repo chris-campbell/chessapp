fd1536108c809b8ae72775ebd63d2c0df1138f49
Kris Camp <christopher.campbell@outlook.com>
2019-01-09 13:14:31 +0000

    Correct invitation_mailer name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
                     format:     { with: VALID_EMAIL_REGEX }
-                    
+
   mount_uploader :avatar, AvatarUploader
-  
+
   after_create :send_email
   def send_email
     NotificationMailer.comment_added(self).deliver
