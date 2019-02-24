72fc5fe629fd365eda003cb0c8265ca953a0ab6e
Chris Camp <christopher.campbell@outlook.com>
2019-02-24 11:38:50 -0500

    Adjust space formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,16 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
                        format:     { with: VALID_EMAIL_REGEX }
-    
+ 
   mount_uploader :avatar, AvatarUploader
-  
+
   # Sent when new user is created in system.
   after_create :send_email
   def send_email
     NotificationMailer.comment_added(self).deliver
   end
-  
-  
+
   def invitation
     InvitationMailer.send_invitation(self).deliver
   end
-
 end
