611072ee5ec0630d8d407d23182bbf4e2fb7f732
Chris Camp <christopher.campbell@outlook.com>
2019-02-24 11:36:04 -0500

    Adjust formatting

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,6 +1,6 @@
 class Queen < Piece
-	def valid_move?(x, y)
-		(position_x == x || position_y == y) || ((position_x - x).abs == (position_y - y).abs)
-		super
-	end
+  def valid_move?(x, y)
+    (position_x == x || position_y == y) || ((position_x - x).abs == (position_y - y).abs)
+    super
+  end
 end
