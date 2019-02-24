78c6c320f92db146ccaa010acafdbbdf07b486a0
Chris Camp <christopher.campbell@outlook.com>
2019-02-24 11:37:41 -0500

    Adjust formatting

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,7 +1,5 @@
 class Rook < Piece
-
-	def valid_move?(x, y)
-		super && ((position_x == x) || (position_y == y))
-	end
-
+  def valid_move?(x, y)
+    super && ((position_x == x) || (position_y == y))
+  end
 end
