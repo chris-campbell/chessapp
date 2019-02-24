36bfda8da8f2354fd44779ca53a8e0c1d1aaefdf
Chris Camp <christopher.campbell@outlook.com>
2019-02-24 11:46:11 -0500

    Adjust space format

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,11 +2,10 @@ class King < Piece
   def valid_move?(x, y)
     super && correct_distance?(x, y)
   end
-  
+
   def correct_distance?(x,y)
     x_distance = (x - position_x).abs
     y_distance = (y - position_y).abs
     (x_distance <= 1) && (y_distance <= 1)
   end
-  
 end
