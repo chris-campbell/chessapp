23e28284375feb5e469acd8b970fd04486fce33e
Ubuntu <ubuntu@ip-172-31-22-85.us-east-2.compute.internal>
2020-01-15 20:44:55 +0000

    Configure action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://fast-wildwood-69169.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://chessmate-saturday-slackers.herokuapp.com' ]
+  config.action_cable.url = 'wss://protected-meadow-37679.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://protected-meadow-37679.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
